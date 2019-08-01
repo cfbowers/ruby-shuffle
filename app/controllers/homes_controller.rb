880c190071e33e5f23ae49dacce9eaa23ce500e2
github username <github email address>
2019-08-01 16:54:02 -0700

    added id key to params. added if statement to check for ids and refactor to integers

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,20 @@
 class HomesController < ApplicationController
 
   def index
-    @movie 
+    @movie
     @genres = Genre.all
   end
 
   def new
-    raise params.inspect
-    genres_array = pa
+    if params[:movie][:genres][:id]
+      genre_id_array = []
+      params[:movie][:genres][:id].each do |g|
+        genre_id_array << g.to_i
+      end
+      raise params.inspect
+    else
+      redirect_to root
+    end
   end
 
   def show

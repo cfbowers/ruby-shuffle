0be080205146bd8fb33ff0b071420ec84d9f44a2
github username <github email address>
2019-08-02 11:31:58 -0700

    added years/keywords to home view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,54 @@
 class HomesController < ApplicationController
 
   def index
-    @movie 
+    @movie
     @genres = Genre.all
   end
 
   def new
-    raise params.inspect
-    genres_array = pa
+
+    if (movie_params[:genres].nil?) && (movie_params[:year_start].empty?) && (movie_params[:year_end].empty?) && (movie_params[:keywords].empty?)
+      redirect_to root_path
+    else
+      genres_array = session[:genres] = []
+      keywords_array = session[:keywords] = []
+      year_array = session[:year] = []
+
+      if !movie_params[:genres].nil?
+        movie_params[:genres][:id].map do |g|
+          genres_array << g.to_i
+        end
+      end
+
+      if !movie_params[:keywords].empty?
+        words_array = movie_params[:keywords].split(", ")
+        words_array.map do |k|
+          keywords_array << k
+        end
+      end
+
+      if !movie_params[:year_start].empty? && !movie_params[:year_end].empty?
+        year_array << movie_params[:year_start]
+        year_array << movie_params[:year_end]
+      elsif !movie_params[:year_start].empty? && movie_params[:year_end].empty?
+        year_array << movie_params[:year_start]
+      elsif movie_params[:year_start].empty? && !movie_params[:year_end].empty?
+        year_array << movie_params[:year_end]
+      end
+      redirect_to homes_show_url
+    end
   end
 
   def show
+    @keywords = session[:keywords]
+    @year = session[:year]
+    @genres = session[:genres]
+  end
+
+  private
+
+  def movie_params
+    params[:movie]
   end
 
 end

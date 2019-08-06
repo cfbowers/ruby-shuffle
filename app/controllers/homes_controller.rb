08865a8340f95ae148e370c0907cb33b3d017356
Derek Cerretani <drkjc@protonmail.com>
2019-08-05 17:53:57 -0700

    removed the new route/moved filters to show route

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
+require 'movie_db'
+
 class HomesController < ApplicationController
 
   def index
@@ -6,51 +8,13 @@ class HomesController < ApplicationController
   end
 
   def new
-    if params[:movie][:genres][:id]
-      genre_id_array = []
-      params[:movie][:genres][:id].each do |g|
-        genre_id_array << g.to_i
-      end
-      raise params.inspect
-    else
-      redirect_to root
 
-    if (movie_params[:genres].nil?) && (movie_params[:year_start].empty?) && (movie_params[:year_end].empty?) && (movie_params[:keywords].empty?)
-      redirect_to root_path
-    else
-      genres_array = session[:genres] = []
-      keywords_array = session[:keywords] = []
-      year_array = session[:year] = []
-
-      if !movie_params[:genres].nil?
-        movie_params[:genres][:id].map do |g|
-          genres_array << g.to_i
-        end
-      end
-
-      if !movie_params[:keywords].empty?
-        words_array = movie_params[:keywords].split(", ")
-        words_array.map do |k|
-          keywords_array << k
-        end
-      end
-
-      if !movie_params[:year_start].empty? && !movie_params[:year_end].empty?
-        year_array << movie_params[:year_start]
-        year_array << movie_params[:year_end]
-      elsif !movie_params[:year_start].empty? && movie_params[:year_end].empty?
-        year_array << movie_params[:year_start]
-      elsif movie_params[:year_start].empty? && !movie_params[:year_end].empty?
-        year_array << movie_params[:year_end]
-      end
-      redirect_to homes_show_url
-    end
   end
 
   def show
-    @keywords = session[:keywords]
-    @year = session[:year]
-    @genres = session[:genres]
+    mdb = Mdb.new
+    @results = mdb.query_movies(movie_params)['results']
+    @base = "https://image.tmdb.org/t/p/w500#{@results[0]['backdrop_path']}"
   end
 
   private
